ec966ba00710a69bd574ac178ca4703e76d19263
Julian <al_jdomenecht@iessierradelasvillas.org>
2022-11-28 10:54:17 +0100

    Examen hecho

---
+++ Examen-Julian.py
@@ -0,0 +1,28 @@
+
+salida=1
+
+
+def imprimir():
+    print("     ")
+    print("Menu")
+    print("1-Sumar")
+    print("2-Salir")
+
+def sumanum(num1,num2):
+    suma=num1+num2
+    return(suma)
+
+while salida!=2:
+    try:
+        imprimir()
+        salida=int(input("Dime la opcion que has elegido: "))
+        if salida==1:
+            num1=int(input("Dime el primer número de la suma: "))
+            num2=int(input("Dime el segundo número de la suma: "))
+            print("La suma es",sumanum(num1,num2))
+    except:
+        print("Tienes que introducir un número")
+
+    
+
+    
